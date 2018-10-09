b16f2cd4d52689d116667f1939b807fa8fb6951c
Andole <andoleal@protonmail.com>
2018-10-09 16:45:43 +0500

    GroupAIBesiege: check_phalanx_spawned

---
+++ groupaistatebesiege.lua
@@ -0,0 +1,15 @@
+-- Prevent the crash on Birth of Sky
+local origfunc = GroupAIStateBesiege._check_phalanx_group_has_spawned
+function GroupAIStateBesiege:_check_phalanx_group_has_spawned(...)
+	if self._phalanx_spawn_group
+		and self._phalanx_spawn_group.has_spawned
+		and not self._phalanx_spawn_group.set_to_phalanx_group_obj
+	then
+		for i, group_unit in pairs(self._phalanx_spawn_group.units) do
+			if group_unit.unit and group_unit.unit:base() then
+				group_unit.unit:base().is_phalanx = true
+			end
+		end
+	end
+	origfunc(self, ...)
+end
