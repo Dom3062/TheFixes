1ce48183cdc6d153b7f248e75be01f27c540a00f
Andole <andoleal@protonmail.com>
2018-10-10 11:29:54 +0500

    DynamicResourceMan: crash when modifying a weapon

---
+++ dynamicresourcemanager.lua
@@ -0,0 +1,9 @@
+local origfunc = DynamicResourceManager.unload
+function DynamicResourceManager:unload(resource_type, resource_name, package_name, keep_using, ...)
+	local key = self._get_resource_key(resource_type, resource_name, package_name)
+	local entry = self._dyn_resources[key]
+	
+	if entry then
+		origfunc(self, resource_type, resource_name, package_name, keep_using, ...)
+	end
+end
