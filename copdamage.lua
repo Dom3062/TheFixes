c3b41cbf6b035f267f6cd0ad7ac1a2c586849c28
Andole <andoleal@protonmail.com>
2018-10-10 15:46:57 +0500

    CopDamage: shotguns prioritize dozer's faceplate and visor

--- copdamage.lua
+++ copdamage.lua
@@ -1,3 +1,4 @@
+-- Fire DOT stats fix
 local origfunc = CopDamage._on_damage_received
 function CopDamage:_on_damage_received(damage_info, ...)
 	if damage_info.is_fire_dot_damage
@@ -13,4 +14,25 @@ function CopDamage:_on_damage_received(damage_info, ...)
 		managers.statistics:killed(data)
 	end
 	return origfunc(self, damage_info, ...)
+end
+
+
+-- Make shotgun pellets prioritize dozer's faceplate and visor
+local origfunc2 = CopDamage.is_head
+function CopDamage:is_head(body, ...)
+	local head = origfunc2(self, body, ...) or false
+	
+	if not head then
+		local bn = tostring(body:name()) --Idstring(@xxxxxxxxxxxxxxxxxx@)
+		local di = (debug.getinfo(3, 'Sn') or {})
+		if di.name and di.name == '_fire_raycast'
+			and di.source and di.source:match('shotgunbase%.lua')
+			and (bn:match('IDf46eb16d189339da')
+				or bn:match('IDf260d73afd0c74fe'))
+		then
+			head = true
+		end
+	end
+	
+	return head
 end
