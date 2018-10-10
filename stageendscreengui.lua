d996c08f7cb8036bb27943f7303663444b11f2e6
Andole <andoleal@protonmail.com>
2018-10-10 23:37:44 +0500

    REMOVED: StageEndScreenGui: 'continue' button always active

--- stageendscreengui.lua
+++
@@ -1,4 +0,0 @@
-local origfunc = StageEndScreenGui.set_continue_button_text
-function StageEndScreenGui:set_continue_button_text(text, not_clickable, ...)
-	return origfunc(self, text, false, ...)
-end
