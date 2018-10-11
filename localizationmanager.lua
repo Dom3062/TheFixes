1d7606fe400f8af9f85b5075799df8d0984843d6
Andole <andoleal@protonmail.com>
2018-10-11 10:51:02 +0500

    ACHIEVEMENT: 'The Red Button' description overkill and above

--- localizationmanager.lua
+++ localizationmanager.lua
@@ -1,5 +1,16 @@
+-- string_id=nil fix
 local text_original = LocalizationManager.text
 function LocalizationManager:text(string_id, ...)
 	if not string_id then string_id = 'NIL' end
 	return text_original(self, string_id, ...)
+end
+
+
+-- 'The Red Button' achievement description
+local origfunc = LocalizationManager.init
+function LocalizationManager:init(...)
+	origfunc(self, ...)
+	LocalizationManager:add_localized_strings({
+		achievement_des_9_desc = self:text('achievement_des_9_desc')..' (OVERKILL+)'
+	})
 end
