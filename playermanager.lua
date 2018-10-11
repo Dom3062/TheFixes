33f138f530816ecb7da931a360f39652f020b11f
Andole <andoleal@protonmail.com>
2018-10-11 11:31:24 +0500

    PlayerManager: player can always interact with a circle_cutter

---
+++ playermanager.lua
@@ -0,0 +1,7 @@
+local origfunc = PlayerManager._can_pickup_special_equipment
+function PlayerManager:_can_pickup_special_equipment(special_equipment, name, ...)
+	if special_equipment.amount and name == 'circle_cutter' then
+		return true
+	end
+	return origfunc(self, special_equipment, name, ...)
+end
