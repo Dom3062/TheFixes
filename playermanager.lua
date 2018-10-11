2ee7c905c6045493ea7b7b65d092772e086e9b63
Andole <andoleal@protonmail.com>
2018-10-11 18:14:34 +0500

    SKILLS: fix for 1 joker overrides 2 jokers

--- playermanager.lua
+++ playermanager.lua
@@ -1,3 +1,4 @@
+-- Always pick up those cutters
 local origfunc = PlayerManager._can_pickup_special_equipment
 function PlayerManager:_can_pickup_special_equipment(special_equipment, name, ...)
 	if special_equipment.amount and name == 'circle_cutter' then
