9c7ad7cc0205460d2865722eca2de1126532a091
Andole <andoleal@protonmail.com>
2018-10-12 15:24:06 +0500

    EcmJammerBase: fix for the position of ECM feedback #2

--- ecmjammerbase.lua
+++ ecmjammerbase.lua
@@ -1,8 +1,10 @@
 -- Make ECM feedback always be around the ECM itself
 local origfunc = ECMJammerBase._detect_and_give_dmg
 function ECMJammerBase._detect_and_give_dmg(hit_pos, device_unit, user_unit, ...)
-	local newpos = device_unit:position()
-	if newpos then
-		origfunc(newpos, device_unit, user_unit, ...)
+	ECMJammerBase.the_fixes_feedback_positions = ECMJammerBase.the_fixes_feedback_positions or {}
+	local key = device_unit:key()
+	if not ECMJammerBase.the_fixes_feedback_positions[key] then
+		ECMJammerBase.the_fixes_feedback_positions[key] = user_unit:position() or Vector3(0, 0, 0)
 	end
+	origfunc(ECMJammerBase.the_fixes_feedback_positions[key], device_unit, user_unit, ...)
 end
