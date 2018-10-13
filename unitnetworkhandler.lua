f873ca3622096dac9bdf5e9f0ddcf4b43b477d76
Andole <andoleal@protonmail.com>
2018-10-13 15:04:22 +0500

    UnitNetworkHandler: unsync the second circle cutter #2

--- unitnetworkhandler.lua
+++ unitnetworkhandler.lua
@@ -4,9 +4,11 @@ function UnitNetworkHandler:special_eq_response(unit, ...)
 	local ui = unit:interaction()
 	if ui._special_equipment
 		and ui._special_equipment == 'circle_cutter'
-		and managers.player:has_special_equipment('circle_cutter') -- if not then it's fixed
 	then
-		return
+		local eq = managers.player:has_special_equipment('circle_cutter')
+		if eq and eq.amount and eq.amount > 0 then -- if not then it's fixed
+			return
+		end
 	end
 	origfunc(self, unit, ...)
 end
