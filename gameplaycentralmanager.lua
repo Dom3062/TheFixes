a0557a8a4ac2308de6afe78f041ee432d6260086
Andole <andoleal@protonmail.com>
2018-10-13 18:27:54 +0500

    GameplayCentralMan: shotgun push fix #2

--- gameplaycentralmanager.lua
+++ gameplaycentralmanager.lua
@@ -1,9 +1,9 @@
 local lastunit = nil
 local origfunc = GamePlayCentralManager._do_shotgun_push
 function GamePlayCentralManager:_do_shotgun_push(unit, ...)
-	if lastunit and lastunit == unit:key() then
+	if lastunit and lastunit == unit then
 		return
 	end
-	lastunit = unit:key()
+	lastunit = unit
 	return origfunc(self, unit, ...)
 end
