be2f283f6c0a7bd758565c555c18f79bf26a7601
Andole <andoleal@protonmail.com>
2018-10-19 22:44:08 +0500

    GroupAIBesiege: check_phalanx_spawned #3

--- groupaistatebesiege.lua
+++ groupaistatebesiege.lua
@@ -6,7 +6,7 @@ function GroupAIStateBesiege:_check_phalanx_group_has_spawned(...)
 		and not self._phalanx_spawn_group.set_to_phalanx_group_obj
 	then
 		for i, group_unit in pairs(self._phalanx_spawn_group.units) do
-			if not (group_unit.unit and group_unit.unit:base()) then
+			if not group_unit.unit then
 				self._phalanx_spawn_group.units[i] = nil
 			end
 		end
