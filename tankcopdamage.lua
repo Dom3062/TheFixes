ec90b1fd6cf20fb097e8de85889ba05eb4bc34ce
Andole <andoleal@protonmail.com>
2018-10-21 18:44:28 +0500

    TankCopDamage: shotguns prioritize dozer's face #2

--- tankcopdamage.lua
+++ tankcopdamage.lua
@@ -4,9 +4,9 @@ function TankCopDamage:is_head(body, ...)
 	local head = origfunc2 and origfunc2(self, body, ...) or TankCopDamage.super.is_head(self, body, ...)
 	
 	if not head and body then
-		local bn = tostring(body:name()) --Idstring(@xxxxxxxxxxxxxxxxxx@)
-		if (bn:match('IDf46eb16d189339da')
-				or bn:match('IDf260d73afd0c74fe'))
+		local bn = body:name():key()
+		if bn == 'f46eb16d189339da'
+			or bn == 'f260d73afd0c74fe'
 		then
 			head = true
 		end
