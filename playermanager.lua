2da3bd26b9872f52746843a95343bdafaef2c1cc
Andole <andoleal@protonmail.com>
2018-10-22 21:51:07 +0500

    PlayerManager: fix for extra ammo boxes spawning for each enemy in a multikill

--- playermanager.lua
+++ playermanager.lua
@@ -6,3 +6,16 @@ function PlayerManager:_can_pickup_special_equipment(special_equipment, name, ..
 	end
 	return origfunc(self, special_equipment, name, ...)
 end
+
+-- Execute second ammo box once the enemy is killed
+--  (not after the bullet finished hitting enemies)
+local origfunc2 = PlayerManager.on_killshot
+function PlayerManager:on_killshot(...)
+	origfunc2(self, ...)
+	
+	if self._message_system
+		and self._message_system.the_fixes_notify_now_by_added_message
+	then
+		self._message_system:the_fixes_notify_now_by_added_message(Message.OnEnemyKilled)
+	end
+end
