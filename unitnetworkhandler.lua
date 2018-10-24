c5a169d71fc4abfc000e86d5428c68c1e8661f40
Andole <andoleal@protonmail.com>
2018-10-24 20:58:13 +0500

    UnitNetWorkHandler: crash with grenade=nil in sync_tear_gas_grenade_properties()

--- unitnetworkhandler.lua
+++ unitnetworkhandler.lua
@@ -4,3 +4,11 @@ function UnitNetworkHandler:sync_unit_converted(unit, ...)
 		return origfunc(self, unit, ...)
 	end
 end
+
+
+local origfunc2 = UnitNetworkHandler.sync_tear_gas_grenade_properties
+function UnitNetworkHandler:sync_tear_gas_grenade_properties(grenade, ...)
+	if grenade and grenade:base() then
+		return origfunc(self, grenade, ...)
+	end
+end
