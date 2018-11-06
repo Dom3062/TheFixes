457f7ee23570655f7d94af6256065666a59bc9d5
Andole <andoleal@protonmail.com>
2018-11-06 17:10:06 +0500

    UnitNetworkHandler: sentry and turret unit=nil checks

--- unitnetworkhandler.lua
+++ unitnetworkhandler.lua
@@ -12,3 +12,30 @@ function UnitNetworkHandler:sync_tear_gas_grenade_properties(grenade, ...)
 		return origfunc(self, grenade, ...)
 	end
 end
+
+
+local orig_names = {
+	'sync_sentrygun_dynamic',
+	'sentrygun_ammo',
+	'sentrygun_sync_armor_piercing',
+	'sync_fire_mode_interaction',
+	'sentrygun_health',
+	'turret_idle_state',
+	'turret_update_shield_smoke_level',
+	'turret_repair',
+	'turret_complete_repairing',
+	'turret_repair_shield'
+}
+
+local origs = {}
+for k,v in pairs(orig_names) do
+	if UnitNetworkHandler[v] then
+		origs[v] = UnitNetworkHandler[v]
+		
+		UnitNetworkHandler[v] = function(this, unit, ...)
+			if unit then
+				origs[v](this, unit, ...)
+			end
+		end
+	end
+end
