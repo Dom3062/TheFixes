94645b971c88b04236b2bdc0d66e430557103e9a
Andole <andoleal@protonmail.com>
2018-11-10 15:31:47 +0500

    NetworkPeer: display mods list for local player

---
+++ networkpeer.lua
@@ -0,0 +1,26 @@
+-- Display mods list for local peer
+local orig_peer_init = NetworkPeer.init
+function NetworkPeer:init(...)
+	orig_peer_init(self, ...)
+	
+	local local_peer = false
+	if self._rpc then
+		if self._rpc:ip_at_index(0) == Network:self("TCP_IP"):ip_at_index(0) then
+			local_peer = true
+		end
+	elseif self._steam_rpc and self._steam_rpc:ip_at_index(0) == Network:self("STEAM"):ip_at_index(0) then
+		local_peer = true
+	end
+	
+	if local_peer
+		and MenuCallbackHandler.build_mods_list
+	then
+		self._mods = self._mods or {}
+		for k,v in ipairs(MenuCallbackHandler:build_mods_list()) do
+			table.insert(self._mods, {
+				id = v[2],
+				name = v[1]
+			})
+		end
+	end
+end
