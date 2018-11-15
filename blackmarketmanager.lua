48bd93688bf1485ad793ce183f8875e49d871291
Andole <andoleal@protonmail.com>
2018-11-15 10:44:40 +0500

    BlackMarketMan: custom van skin crash fix #2

--- blackmarketmanager.lua
+++ blackmarketmanager.lua
@@ -83,7 +83,7 @@ end
 
 -- Custom van skins ? crash fix
 local van_skin_orig = BlackMarketManager.equipped_van_skin
-BlackMarketManager:equipped_van_skin(...)
+function BlackMarketManager:equipped_van_skin(...)
 	local res = van_skin_orig(self, ...) or ''
 	
 	if not tweak_data.van.skins[res] then
