1f5af53cd2db6c910de08575a7f0d77320931393
Andole <andoleal@protonmail.com>
2018-11-21 21:38:21 +0500

    TimerGui: crash with self._current_timer=nil in TimerGui:_set_jammed()

---
+++ timergui.lua
@@ -0,0 +1,10 @@
+-- Something will glitch out, but the game won't crash
+local set_jammed_orig = TimerGui._set_jammed
+function TimerGui:_set_jammed(jammed, ...)
+	-- 565: bad argument #1 to 'floor' (number expected, got nil)
+	if not jammed and not self._current_timer then
+		return
+	end
+	
+	set_jammed_orig(self, jammed, ...)
+end
