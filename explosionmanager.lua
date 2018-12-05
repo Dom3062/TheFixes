10d1b25ec210c869b6d62a63edd5c43bd7ad85f7
Andole <andoleal@protonmail.com>
2018-12-05 20:20:00 +0500

    ExplosionMan: the crash with detect_and_give_dmg() returning nil as the 3rd value (a mod issue?)

---
+++ explosionmanager.lua
@@ -0,0 +1,7 @@
+local detect_give_dmg = ExplosionManager.detect_and_give_dmg
+function ExplosionManager:detect_and_give_dmg(...)
+	local res1, res2, res3 = detect_give_dmg(self, ...)
+	res3 = res3 or {}
+	
+	return res1, res2, res3
+end
