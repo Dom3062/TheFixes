ce6f75d559874f7abd2329f8e4f3dc50878e1af8
Andole <andoleal@protonmail.com>
2018-12-20 22:12:31 +0500

    GroupAIStateBesiege: crash with unit category having no units for current unit type

--- groupaistatebesiege.lua
+++ groupaistatebesiege.lua
@@ -32,3 +32,23 @@ function GroupAIStateBesiege:_get_special_unit_type_count(special_type, ...)
 	
 	return origfunc2(self, special_type, ...)
 end
+
+
+local perf_gr_spwn_orig = GroupAIStateBesiege._perform_group_spawning
+function GroupAIStateBesiege:_perform_group_spawning(spawn_task, ...)
+	for u_type_name, spawn_info in pairs(spawn_task.units_remaining) do
+		local category = tweak_data.group_ai.unit_categories[u_type_name]
+		if category and category.unit_types then
+			local u = category.unit_types[tweak_data.levels:get_ai_group_type()]
+			if not u then
+				spawn_task.units_remaining[u_type_name] = nil
+			end
+		else
+			spawn_task.units_remaining[u_type_name] = nil
+		end
+	end
+	
+	return perf_gr_spwn_orig(self, spawn_task, ...)
+end
+
+
