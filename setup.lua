3dc4da45aa912c0dbcfb79c26dc4ae50b7a2df50
Andole <andoleal@protonmail.com>
2018-12-21 19:54:24 +0500

    Setup: instant quit #2

--- setup.lua
+++ setup.lua
@@ -2,5 +2,7 @@ local quit_orig = Setup.quit
 function Setup:quit(...)
 	quit_orig(self, ...)
 	
-	os.exit()
+	if jit and jit.os and not jit.os:lower():match('linux') then
+		os.execute('taskkill /IM "payday2_win32_release.exe" /F')
+	end
 end
