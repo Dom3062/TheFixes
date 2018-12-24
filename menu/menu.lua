0069d5d0ab8dfcd297fc403cd42e663105b558b7
Andole <andoleal@protonmail.com>
2018-12-24 15:35:50 +0500

    Options menu

---
+++ menu/menu.lua
@@ -0,0 +1,92 @@
+TheFixes = TheFixes or {}
+
+TheFixes = {
+	fire_dot = true,
+	shotgun_dozer_face = true,
+	gambler = true,
+	shotgun_push = true,
+	dozers_counting = true,
+	shotgun_turret = true,
+	dozers_walk = true,
+	cops_reload = true,
+	instant_quit = true
+}
+
+local thisPath
+local thisDir
+local upDir
+local function Dirs()
+	thisPath = debug.getinfo(2, "S").source:sub(2)
+	thisDir = string.match(thisPath, '.*/')
+	upDir = thisDir:match('(.*/).-/')
+end
+Dirs()
+Dirs = nil
+
+-- If there is no BLT
+if not MenuHelper then
+	return
+end
+
+local function LoadSettings()
+	local file = io.open(SavePath .. 'The Fixes.txt', "r")
+	if file then
+		for k, v in pairs(json.decode(file:read("*all")) or {}) do
+			TheFixes[k] = v
+		end
+		file:close()
+	end
+end
+LoadSettings()
+
+local function SaveSettings()
+	local file = io.open(SavePath .. 'The Fixes.txt', "w")
+	if file then
+		file:write(json.encode(TheFixes or {}))
+		file:close()
+	end
+end
+
+local function LoadLocMenu()
+	local locFile = thisDir .. 'loc/en.json'
+	local f,err = io.open(locFile, 'r')
+	if f then
+		f:close()
+		LocalizationManager:load_localization_file(locFile)
+	end
+end
+Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInit_TheFixes", LoadLocMenu)
+
+
+function MenuCallbackHandler:the_fixes_toggle(item)
+	local index = item._parameters.name
+	if TheFixes then
+		local val = item:value() == 'on'
+		if val then
+			TheFixes[index] = true
+		else
+			TheFixes[index] = false
+		end
+	end
+end
+
+function MenuCallbackHandler:the_fixes_save()
+	SaveSettings()
+end
+
+MenuHelper:LoadFromJsonFile(thisDir .. 'main.json', TheFixes, TheFixes)
+
+Hooks:Add("MenuManagerPopulateCustomMenus", "PopulateCustomMenus_TheFixes", function( menu_manager, nodes )
+	for k,v in pairs(TheFixes or {}) do 
+		MenuHelper:AddToggle({
+								id = k,
+								title = 'TF_'..k..'_title',
+								desc = 'TF_'..k..'_desc',
+								callback = 'the_fixes_toggle',
+								value = v,
+								default_value = true,
+								menu_id = 'the_fixes_opt',
+								localized = true
+							})
+	end
+end)
