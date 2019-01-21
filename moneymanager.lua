ca537f0f60d36a1d746808861fd107044735274e
Andole <andoleal@protonmail.com>
2019-01-21 21:01:28 +0500

    MoneyManager:1072: table index is nil

--- moneymanager.lua
+++ moneymanager.lua
@@ -3,3 +3,8 @@ function MoneyManager:load(data, ...)
 	data.MoneyManager = data.MoneyManager or {total=1000000, total_collected = 0, offshore=10000000, total_spent=0}
 	return origfunc(self, data, ...)
 end
+
+local get_craft_price_orig = MoneyManager.get_mask_crafting_price
+function MoneyManager:get_mask_crafting_price(mask_id, global_value, ...)
+	return get_craft_price_orig(self, mask_id, global_value or "normal", ...)
+end
