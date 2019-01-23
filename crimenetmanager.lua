752d3e7ab5ac9d1026cd6c6e89663eb8b19ac628
Andole <andoleal@protonmail.com>
2019-01-23 11:54:19 +0500

    CrimeNetManager: fix for get_last_played_job() returning 0 sometimes

---
+++ crimenetmanager.lua
@@ -0,0 +1,9 @@
+-- Because the original can return 0, which immediately gives 'attempt to index a number'
+local get_last_pl_orig = CrimeNetManager.get_last_played_job
+function CrimeNetManager:get_last_played_job(...)
+	local res = get_last_pl_orig(self, ...)
+	if type(res) ~= 'table' then
+		return DateTime:new("now")
+	end
+	return res
+end
