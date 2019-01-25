6a4446cad9d39bbc711e382767eddf05e5cad24a
Andole <andoleal@protonmail.com>
2019-01-25 11:16:48 +0500

    CivilianLogicTravel: don't do anything if 'Please, Go There' mod is present.

--- civilianlogictravel.lua
+++ civilianlogictravel.lua
@@ -1,3 +1,13 @@
+if BLT and BLT.Mods then
+	local mods = BLT.Mods:Mods()
+	for k,v in pairs(mods) do
+		if v.name and v.name:lower() == 'please, go there' then
+			log('[The Fixes] CivilianLogicTravel disabled')
+			return
+		end
+	end
+end
+
 -- Make civilians go straight to the player they are following
 local origfunc = CivilianLogicTravel._determine_exact_destination
 function CivilianLogicTravel._determine_exact_destination(data, objective, ...)
