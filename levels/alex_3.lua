ea47717892ed9a3d529b3c9c37a70e6fda9478f3
Andole <andoleal@protonmail.com>
2019-01-27 20:17:47 +0500

    Rats day 3: add a collision

---
+++ levels/alex_3.lua
@@ -0,0 +1,4 @@
+-- Add a collision to prevent players from getting out of playable area
+
+local collision = safe_spawn_unit(Idstring('units/dev_tools/level_tools/collision/dev_collision_50m/dev_collision_50m'), Vector3(19272, 22983, 1906), Rotation(90,0,0))
+collision:set_visible(false)
