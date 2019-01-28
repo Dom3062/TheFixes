16d3ac3a042dc41db875ed2e8c5355b17ec1c3cd
Andole <andoleal@protonmail.com>
2019-01-28 13:25:23 +0500

    Goat simulator day 2: fix for assets spawning multiple times

---
+++ levels/peta2.lua
@@ -0,0 +1,18 @@
+-- This is supposed to fix the issue where assets spawn multiple times
+local on_exec_orig = ElementSpawnDeployable.on_executed
+local spawned = {}
+function ElementSpawnDeployable:on_executed(...)
+	if not self._values.enabled then
+		return
+	end
+	
+	if self._values.deployable_id and self._values.deployable_id ~= 'none' then
+		if spawned[self._values.deployable_id]
+			and spawned[self._values.deployable_id] == self._values.position
+		then
+			return
+		end
+	end
+	spawned[self._values.deployable_id] = self._values.position
+	on_exec_orig(self, ...)
+end
