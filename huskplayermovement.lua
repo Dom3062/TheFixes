84d926a59c972a5339514063a14bfcd6059055be
Andole <andoleal@protonmail.com>
2019-02-16 15:14:54 +0500

    HuskPlayerMovement: 4288: attempt to perform arithmetic on field 'equip' (a nil value)

--- huskplayermovement.lua
+++ huskplayermovement.lua
@@ -19,3 +19,15 @@ local sync_melee_orig = HuskPlayerMovement.sync_melee_start
 function HuskPlayerMovement:sync_melee_start(hand, ...)
 	sync_melee_orig(self, hand or 0, ...)
 end
+
+
+local eq_weap_td_orig = HuskPlayerMovement._equipped_weapon_tweak_data
+function HuskPlayerMovement:_equipped_weapon_tweak_data(...)
+	local res = eq_weap_td_orig(self, ...)
+	if res then
+		res.timers = res.timers or {}
+		res.timers.equip = res.timers.equip or 0.5
+		res.timers.unequip = res.timers.unequip or 0.5
+	end
+	return res
+end
