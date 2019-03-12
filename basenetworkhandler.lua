3402f5e467adbff3b85b6667c3de7e90a185ac01
Andole <andoleal@protonmail.com>
2019-03-12 14:14:27 +0500

    BaseNetworkHandler: 69: attempt to index a nil value (rpc = nil)

---
+++ basenetworkhandler.lua
@@ -0,0 +1,8 @@
+TheFixesPreventer = TheFixesPreventer or {}
+if not TheFixesPreventer.crash_ver_char_send_basenetwork then
+	local verify_char_send_orig = BaseNetworkHandler._verify_character_and_sender
+	function BaseNetworkHandler._verify_character_and_sender(unit, rpc, ...)
+		if not rpc then return false end
+		return verify_char_send_orig(unit, rpc, ...)
+	end
+end
