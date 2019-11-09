f9d5e0858bed47a5f2b5fc7ba2da218eb8edd43d
Andole <andoleal@protonmail.com>
2019-11-09 19:10:30 +0500

    Removed bootupstate

--- bootupstate.lua
+++
@@ -1,8 +0,0 @@
-TheFixesPreventer = TheFixesPreventer or {}
-if not TheFixesPreventer.crash_bootup_play_next then
-	local origfunc = BootupState.play_next
-	function BootupState:play_next(...)
-		origfunc(self, ...)
-		self._play_data = self._play_data or {}
-	end
-end
