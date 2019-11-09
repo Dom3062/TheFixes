0bf1400381b14074c6a9b9afce17a6c42a9193cd
Andole <andoleal@protonmail.com>
2019-11-09 11:23:01 +0500

    Removed chill.lua

--- levels/chill.lua
+++
@@ -1,6 +0,0 @@
-TheFixesPreventer = TheFixesPreventer or {}
-if TheFixesPreventer.heist_safehouse_wall_cover then
-	return
-end
-
-safe_spawn_unit(Idstring('units/payday2/props/air_prop_runway_fence/air_prop_runway_fence_cloth'), Vector3(-12, 2001, -421), Rotation(0,0,0))
