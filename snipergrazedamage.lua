ef85a792f1d56ba89553258b466ec25ca6be7b6e
Andole <andoleal@protonmail.com>
2019-11-09 19:12:16 +0500

    Removed snipergrazedamage

--- snipergrazedamage.lua
+++
@@ -1,9 +0,0 @@
-TheFixesPreventer = TheFixesPreventer or {}
-if not TheFixesPreventer.crash_weap_fired_snipergrazedamage then
-	local origfunc = SniperGrazeDamage.on_weapon_fired
-	function SniperGrazeDamage:on_weapon_fired(weapon_unit, result, ...)
-		if weapon_unit and result then
-			origfunc(self, weapon_unit, result, ...)
-		end
-	end
-end
