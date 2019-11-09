187c3cbbb02bd5808fa89880ed00559e83954c64
Andole <andoleal@protonmail.com>
2019-11-09 10:06:52 +0500

    Skirmishmanager - removed holdout_rewards_clients

--- skirmishmanager.lua
+++ skirmishmanager.lua
@@ -1,15 +1,4 @@
 TheFixesPreventer = TheFixesPreventer or {}
-if not TheFixesPreventer.holdout_rewards_clients then
-	-- Fix for houldout not rewarding clients who were in the lobby before the host switched to holdout
-	local origfunc = SkirmishManager.init_finalize
-	function SkirmishManager:init_finalize(...)
-		local lobby_data = managers.network.matchmake:get_lobby_data() or {}
-		Global.game_settings.weekly_skirmish = (tonumber(lobby_data.skirmish or '') or 0) == (SkirmishManager.LOBBY_WEEKLY or 10)
-		
-		origfunc(self, ...)
-	end
-end
-
 if not TheFixesPreventer.holdout_rewards_clients_2 then
 	local host_match_orig = SkirmishManager.host_weekly_match
 	function SkirmishManager:host_weekly_match(...)
