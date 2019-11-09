1e71fd044b571d11094e28a86142281a2a7a6ad3
Andole <andoleal@protonmail.com>
2019-11-09 19:13:45 +0500

    Moneymanager - removed crash_get_craft_price_moneyman

--- moneymanager.lua
+++ moneymanager.lua
@@ -6,10 +6,3 @@ if not TheFixesPreventer.crash_money_data_moneyman then
 		return origfunc(self, data, ...)
 	end
 end
-
-if not TheFixesPreventer.crash_get_craft_price_moneyman then
-	local get_craft_price_orig = MoneyManager.get_mask_crafting_price
-	function MoneyManager:get_mask_crafting_price(mask_id, global_value, ...)
-		return get_craft_price_orig(self, mask_id, global_value or "normal", ...)
-	end
-end
