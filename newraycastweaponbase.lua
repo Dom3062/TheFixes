13c7f44e389d7ae612467d6d6ae9a4e260d9ca1f
Andole <andoleal@protonmail.com>
2019-11-09 19:09:12 +0500

    Removed newraycastweaponbase

--- newraycastweaponbase.lua
+++
@@ -1,8 +0,0 @@
-TheFixesPreventer = TheFixesPreventer or {}
-if not TheFixesPreventer.crash_weap_gadgets_newraycast then
-	local origfunc = NewRaycastWeaponBase.get_all_override_weapon_gadgets
-	function NewRaycastWeaponBase:get_all_override_weapon_gadgets(...)
-		local res = origfunc(self, ...) or {}
-		return res
-	end
-end
