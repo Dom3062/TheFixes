ac39da0c8467ff068305657f3ca69c5be9afc214
Andole <andoleal@protonmail.com>
2019-11-09 19:07:50 +0500

    Raycastweaponbase - removed crash_weap_td_raycastbase

--- raycastweaponbase.lua
+++ raycastweaponbase.lua
@@ -1,11 +1,4 @@
 TheFixesPreventer = TheFixesPreventer or {}
-if not TheFixesPreventer.crash_weap_td_raycastbase then
-	local origfunc = RaycastWeaponBase.weapon_tweak_data
-	function RaycastWeaponBase:weapon_tweak_data(...)
-		return origfunc(self, ...) or tweak_data.weapon.ak5 or {}
-	end
-end
-
 if not TheFixesPreventer.crash_create_setups_raycastbase then
 	local cr_use_setups_orig = RaycastWeaponBase._create_use_setups
 	function RaycastWeaponBase:_create_use_setups(...)
