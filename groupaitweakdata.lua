a27c8e5ec328a0505c4abb54e8fb9fdccefd4892
Andole <andoleal@protonmail.com>
2021-02-07 03:00:51 +0500

    Fix crash with absent tweak data in GroupAITweakData:_read_mission_preset

---
+++ groupaitweakdata.lua
@@ -0,0 +1,9 @@
+TheFixesPreventer = TheFixesPreventer or {}
+if not TheFixesPreventer.crash_mission_preset_groupaitweak then
+	local read_mis_preset_orig = GroupAITweakData._read_mission_preset
+    function GroupAITweakData:_read_mission_preset(tweak_data, ...)
+        if tweak_data.levels[Global.game_settings.level_id] then
+            read_mis_preset_orig(self, tweak_data, ...)
+        end
+    end
+end
