8f01f383f9bc6c000b5be825199c7e19a78398b1
Andole <andoleal@protonmail.com>
2021-02-13 00:11:39 +0500

    Add a nil check for groupaitweakdata

--- groupaitweakdata.lua
+++ groupaitweakdata.lua
@@ -2,7 +2,7 @@ TheFixesPreventer = TheFixesPreventer or {}
 if not TheFixesPreventer.crash_mission_preset_groupaitweak then
 	local read_mis_preset_orig = GroupAITweakData._read_mission_preset
     function GroupAITweakData:_read_mission_preset(tweak_data, ...)
-        if tweak_data.levels[Global.game_settings.level_id] then
+        if Global.game_settings and tweak_data.levels[Global.game_settings.level_id] then
             read_mis_preset_orig(self, tweak_data, ...)
         end
     end
