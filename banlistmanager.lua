64caeea652f949608ef52358772ba363b0163e57
Andole <andoleal@protonmail.com>
2021-04-18 00:34:45 +0500

    Fix save data crash in BanListManager

--- banlistmanager.lua
+++ banlistmanager.lua
@@ -9,9 +9,11 @@ if not TheFixesPreventer.crash_ban_list_encoding_banlistman then
             if data.ban_list and data.ban_list.banned then
                 local filtered = {}
                 for k,v in pairs(data.ban_list.banned) do
-                    filtered[k] = v
-                    if not (v.name and TheFixesLib.utf8_validator(v.name)) then
-                        filtered[k].name = "[unknown]"
+                    if type(v) == 'table' then
+                        filtered[k] = v
+                        if not (v.name and TheFixesLib.utf8_validator(v.name)) then
+                            filtered[k].name = "[unknown]"
+                        end
                     end
                 end
                 data.ban_list.banned = filtered
