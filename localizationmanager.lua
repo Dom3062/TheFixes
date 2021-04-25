1f04cacf4d933fd8977389aa812bb8046b3349e4
Andole <andoleal@protonmail.com>
2021-04-26 01:44:06 +0500

    Remove achievement description fixes for infamy achievements and euro bag simulator

--- localizationmanager.lua
+++ localizationmanager.lua
@@ -17,20 +17,6 @@ function LocalizationManager:init(...)
 	})
 	end
 	
-	if not TheFixesPreventer.achi_infamy_localeman then
-		local infamy_strs = {}
-		for i=1, 25 do
-			infamy_strs['achievement_ignominy_'..i..'_desc'] = 'Reach Infamy '..i
-		end
-		LocalizationManager:add_localized_strings(infamy_strs)
-	end
-	
-	if not TheFixesPreventer.achi_euro_bag_sim_localeman then
-	LocalizationManager:add_localized_strings({
-		achievement_cane_3_unlock = self:text('achievement_cane_3_unlock'):gsub('Rudolph','Rudelf')
-	})
-	end
-	
 	if not TheFixesPreventer.trophy_spooky_pumpkin_localeman then
 	LocalizationManager:add_localized_strings({
 		trophy_spooky_objective = self:text('trophy_spooky_objective')..' (HOST ONLY)'
