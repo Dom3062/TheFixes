cb099f93456bd26c0ce2651fd4f760cf113b6873
Andole <andoleal@protonmail.com>
2021-05-02 00:56:32 +0500

    Fix fire dot stats fix

--- copdamage.lua
+++ copdamage.lua
@@ -1,23 +1,9 @@
 TheFixesPreventer = TheFixesPreventer or {}
--- Fire DOT stats fix
 -- Fix for 'Masterpiece' achievement
 -- Fix for 'Matrix with lasers' achievement
 local origfunc = CopDamage._on_damage_received
 function CopDamage:_on_damage_received(damage_info, ...)
 	if damage_info.result.type == 'death' then
-		if damage_info.is_fire_dot_damage
-			and (not TheFixes or TheFixes.fire_dot)
-		then
-			local data = {
-				name = self._unit:base()._tweak_table,
-				stats_name = self._unit:base()._stats_name,
-				head_shot = false,
-				weapon_unit = damage_info.weapon_unit,
-				variant = damage_info.variant
-			}
-			managers.statistics:killed(data)
-		end
-		
 		if not TheFixesPreventer.achi_masterpiece then
 			AchievmentManager.the_fixes_failed = AchievmentManager.the_fixes_failed or {}
 			AchievmentManager.the_fixes_failed['cac_19'] = true
