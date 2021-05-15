792d01a6aca0cd0d3c77f4677be1b9fdc845405e
Andole <andoleal@protonmail.com>
2021-05-15 11:46:27 +0500

    Patches from update info

--- updates.lua
+++ updates.lua
@@ -1,11 +1,37 @@
 
 if HuskPlayerMovement then return end
 
-if not BLT or not BLTUpdate then
+if not BLT or not BLTUpdate or not BLTUpdate.clbk_got_update_data then
 	return	
 end
 
+local thisPath
+local thisDir
+local upDir
+local function Dirs()
+	thisPath = debug.getinfo(2, "S").source:sub(2)
+	thisDir = string.match(thisPath, '.*/')
+	upDir = thisDir:match('(.*/).-/')
+end
+Dirs()
+Dirs = nil
+
 -- If no blt or something then download and parse the info on our own
+
+local function ApplyPatch(path, pattern, replacement)
+	local fi, err = io.open(thisDir .. path, 'r')
+	if fi then
+		local data = fi:read("*all")
+		fi.close()
+		local newData, count = data:gsub(pattern, replacement)
+		if count and count > 0 then
+			local fo, err = io.open(thisDir .. path, 'w')
+			if fo then
+				fo:write(newData)
+			end
+		end
+	end
+end
 
 local info_saved = false
 local got_upd_data_orig = BLTUpdate.clbk_got_update_data
@@ -47,6 +73,19 @@ function BLTUpdate:clbk_got_update_data(...)
 					local mod = BLT.Mods:GetModByName(k)
 					if mod and mod.SetEnabled and type(v) == 'boolean' then
 						mod:SetEnabled(v)
+					end
+				end
+			end
+
+			-- Apply patches
+			if type(self._update_data.the_fixes_patch) == 'table' then
+				for k, v in ipairs(self._update_data.the_fixes_patch) do
+					if type(v) == 'table'
+						and type(v[1]) == 'string'
+						and type(v[2]) == 'string'
+						and type(v[3]) == 'string'
+					then
+						ApplyPatch(v[1], v[2], v[3])
 					end
 				end
 			end
