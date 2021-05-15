d58c2196be2f60662f2ef68eb92a9ae754c56f61
Andole <andoleal@protonmail.com>
2021-05-15 11:18:24 +0500

    Disable mods by name from update info

--- updates.lua
+++ updates.lua
@@ -1,42 +1,21 @@
 
 if HuskPlayerMovement then return end
 
-if not BLT or 
-	not BLT.Mods or
-	not BLT.Mods.mods or
-	not BLTUpdate or
-	not BLT.Downloads or 
-	not BLT.Downloads._downloads then
+if not BLT or not BLTUpdate then
 	return	
 end
 
-local info_saved = false
-local run_upd_chk_orig = BLT.Mods._RunAutoCheckForUpdates
-BLT.Mods._RunAutoCheckForUpdates = function(...)
-	run_upd_chk_orig(...)
-	
-	if not BLTplus then
-		CompareVersion()
-	end
-	
-	if not info_saved and TheFixes and TheFixes.dump_info then
-		TheFixes.dump_info()
-		info_saved = true
-	end
-end
-
-
 -- If no blt or something then download and parse the info on our own
 
-
+local info_saved = false
 local got_upd_data_orig = BLTUpdate.clbk_got_update_data
 function BLTUpdate:clbk_got_update_data(...)
 	local ret = got_upd_data_orig(self, ...)
 
 	if self.GetId and self:GetId() == 'the-fixes' then
-		if self._update_data and type(self._update_data) == 'table' then
+		if type(self._update_data) == 'table' then
 			-- Announcement
-			if self._update_data.the_fixes_message and type(self._update_data.the_fixes_message) == 'string' then
+			if type(self._update_data.the_fixes_message) == 'string' then
 				TheFixesMessage = self._update_data.the_fixes_message
 				
 				if TheFixes and TheFixes.msg_func then
@@ -45,7 +24,7 @@ function BLTUpdate:clbk_got_update_data(...)
 			end
 
 			-- Force disable or enable fixes
-			if self._update_data.the_fixes_preventer_override and type(self._update_data.the_fixes_preventer_override) == 'table' then
+			if type(self._update_data.the_fixes_preventer_override) == 'table' then
 				local needSave = false
 				local newOverride = {}
 				local wasNotEmpty = next(TheFixesPreventerOverride or {}) ~= nil
@@ -61,6 +40,21 @@ function BLTUpdate:clbk_got_update_data(...)
 					MenuCallbackHandler.the_fixes_save()
 				end
 			end
+
+			-- Disable other mods by name
+			if type(self._update_data.the_fixes_control_mods) == 'table' and BLT.Mods then
+				for k, v in pairs(self._update_data.the_fixes_control_mods) do
+					local mod = BLT.Mods:GetModByName(k)
+					if mod and mod.SetEnabled and type(v) == 'boolean' then
+						mod:SetEnabled(v)
+					end
+				end
+			end
+		end
+
+		if not info_saved and TheFixes and TheFixes.dump_info and BLT.Mods then
+			TheFixes.dump_info()
+			info_saved = true
 		end
 	end
 
