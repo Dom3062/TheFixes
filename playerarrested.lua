70ad4c785a7d9fe0a94f0364c541b03b4a111a1b
Andole <andoleal@protonmail.com>
2021-07-10 19:15:06 +0500

    playerarrested..."]:210: attempt to index a nil

--- playerarrested.lua
+++ playerarrested.lua
@@ -1,13 +1,19 @@
 TheFixesPreventer = TheFixesPreventer or {}
 if not TheFixesPreventer.crash_call_teammate_playerarrested then
 	local origfunc = PlayerArrested.call_teammate
-	function PlayerArrested:call_teammate(...)
+	function PlayerArrested:call_teammate(line, t, no_gesture, skip_alert, skip_mark_cop, ...)
 		local vt, plural, prime_target = self:_get_unit_intimidation_action(true, false, true, true, false)
 		
-		if not prime_target and (vt == 'come' or vt == 'mark_cop') then
-			return
+		if vt == 'come' or (vt == 'mark_cop' and not skip_mark_cop) then
+			if not (prime_target and prime_target.unit) then
+				return
+			end
+
+			if not tweak_data.character[prime_target.unit:base()._tweak_table] then
+				return
+			end
 		end
 		
-		origfunc(self, ...)
+		origfunc(self, line, t, no_gesture, skip_alert, skip_mark_cop, ...)
 	end
 end
