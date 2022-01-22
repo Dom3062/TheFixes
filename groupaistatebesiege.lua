224eb40079c8ef4a39a12659989d78e00d69d5b5
Andole <andoleal@protonmail.com>
2022-01-22 20:20:20 +0500

    Remove crash_winters_bos_aistatebesiege

--- groupaistatebesiege.lua
+++ groupaistatebesiege.lua
@@ -1,23 +1,4 @@
 TheFixesPreventer = TheFixesPreventer or {}
-if not TheFixesPreventer.crash_winters_bos_aistatebesiege then
-	-- Prevent the crash on Birth of Sky
-	local origfunc = GroupAIStateBesiege._check_phalanx_group_has_spawned
-	function GroupAIStateBesiege:_check_phalanx_group_has_spawned(...)
-		if self._phalanx_spawn_group
-			and self._phalanx_spawn_group.has_spawned
-			and not self._phalanx_spawn_group.set_to_phalanx_group_obj
-		then
-			for i, group_unit in pairs(self._phalanx_spawn_group.units) do
-				if not group_unit.unit then
-					self._phalanx_spawn_group.units[i] = nil
-				end
-			end
-		end
-		origfunc(self, ...)
-	end
-end
-
-
 -- Fix for the bug when there is too many dozers
 local fixed = false
 local origfunc2 = GroupAIStateBesiege._get_special_unit_type_count
