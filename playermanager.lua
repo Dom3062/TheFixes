afb645f98ae1bf9b0f873d5b917bf1f78f66a4fd
Andole <andoleal@protonmail.com>
2022-01-22 20:21:09 +0500

    Remove remove_bag_from_back_playerman

--- playermanager.lua
+++ playermanager.lua
@@ -25,36 +25,6 @@ if not TheFixesPreventer.sixth_ammo_box_playerman then
 	end
 end
 
-if not TheFixesPreventer.remove_bag_from_back_playerman then
-	-- If someone throws a bag then remove it from his back and from hud
-	local origfunc3 = PlayerManager.sync_carry_data
-	function PlayerManager:sync_carry_data(unit, carry_id, carry_multiplier, dye_initiated, has_dye_pack, dye_value_multiplier, position, dir, throw_distance_multiplier_upgrade_level, zipline_unit, peer_id, ...)
-		origfunc3(self, unit, carry_id, carry_multiplier, dye_initiated, has_dye_pack, dye_value_multiplier, position, dir, throw_distance_multiplier_upgrade_level, zipline_unit, peer_id, ...)
-		
-		self._global.synced_carry[peer_id] = nil
-		
-		if managers.network:session():local_peer():id() == peer_id then
-			return
-		end
-		
-		local pl_unit = managers.network:session():peer(peer_id)
-		local hud_pnl = pl_unit and managers.hud:get_teammate_panel_by_peer(pl_unit) or nil
-		
-		if pl_unit then
-			managers.hud:remove_name_label_carry_info(pl_unit:id() or 0)
-		end
-		
-		pl_unit = pl_unit and pl_unit:unit() or nil
-		if pl_unit then
-			pl_unit:movement():_destroy_current_carry_unit()
-			
-			if hud_pnl then
-				hud_pnl:remove_carry_info()
-			end
-		end
-	end
-end
-
 if not TheFixesPreventer.set_equipment_playerman then
 	local set_equipment_orig = PlayerManager.set_synced_equipment_possession
 	function PlayerManager:set_synced_equipment_possession(peer_id, equipment, ...)
