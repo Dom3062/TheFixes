9e8a5c93717e27e2196024e0b857981451903beb
Andole <andoleal@protonmail.com>
2022-01-29 22:25:50 +0500

    Remove crash_check_act_shock_playertased

--- playertased.lua
+++
@@ -1,8 +0,0 @@
-TheFixesPreventer = TheFixesPreventer or {}
-if not TheFixesPreventer.crash_check_act_shock_playertased then
-    local check_act_shock_orig = PlayerTased._check_action_shock
-    function PlayerTased:_check_action_shock(...)
-        self._next_shock = self._next_shock or 0.5
-        return check_act_shock_orig(self, ...)
-    end
-end
