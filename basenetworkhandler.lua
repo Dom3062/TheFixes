ac951ad21fcc4b80d1fe27562b5443c2e091964f
Andole <andoleal@protonmail.com>
2022-06-04 15:15:23 +0500

    Remove basenetworkhandler

--- basenetworkhandler.lua
+++
@@ -1,9 +0,0 @@
-TheFixesPreventer = TheFixesPreventer or {}
-if not TheFixesPreventer.crash_ver_char_send_basenetwork then
-	local verify_char_send_orig = BaseNetworkHandler._verify_character_and_sender
-	function BaseNetworkHandler._verify_character_and_sender(unit, rpc, ...)
-		if not rpc then return false end
-		if unit and not unit:character_damage() then return false end
-		return verify_char_send_orig(unit, rpc, ...)
-	end
-end
