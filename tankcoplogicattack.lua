7c634fe1c7f7e455ae4b90f13e022d7ed9650863
Dom <38251670+Dom3062@users.noreply.github.com>
2022-06-24 13:33:09 +0200

    Moved option to Preventer

--- tankcoplogicattack.lua
+++ tankcoplogicattack.lua
@@ -1,8 +1,11 @@
--- Make dozers walk when near heisters
-local origfunc = TankCopLogicAttack._chk_request_action_walk_to_chase_pos
-function TankCopLogicAttack._chk_request_action_walk_to_chase_pos(data, my_data, speed, ...)
-	local run_dist = data.attention_obj.verified and 1500 or 800
-	local walk = data.attention_obj.verified_dis < run_dist and (not TheFixes or TheFixes.dozers_walk)
-	
-	return origfunc(data, my_data, walk and 'walk' or speed, ...)
+TheFixesPreventer = TheFixesPreventer or {}
+if not TheFixesPreventer.tank_walk_near_players then
+	-- Make dozers walk when near heisters
+	local origfunc = TankCopLogicAttack._chk_request_action_walk_to_chase_pos
+	function TankCopLogicAttack._chk_request_action_walk_to_chase_pos(data, my_data, speed, ...)
+		local run_dist = data.attention_obj.verified and 1500 or 800
+		local walk = data.attention_obj.verified_dis < run_dist
+		
+		return origfunc(data, my_data, walk and 'walk' or speed, ...)
+	end
 end
