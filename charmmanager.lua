3c2a4dc7c461fb7e8c01d057ae507164549078a2
Dom <38251670+Dom3062@users.noreply.github.com>
2022-10-28 18:30:29 +0200

    Update charmmanager.lua

--- charmmanager.lua
+++ charmmanager.lua
@@ -2,25 +2,27 @@ TheFixesPreventer = TheFixesPreventer or {}
 if not TheFixesPreventer.crash_charmmanager_missing_player_camera_extension then
     local original = CharmManager.get_movement_data
     function CharmManager:get_movement_data(weapon, user, is_menu, ...)
-        local base_ext = user:base()
-        if user and base_ext and base_ext.is_local_player then
-            if user:camera() then -- Camera extension is present, run original function because it won't crash
-                return original(self, weapon, user, is_menu, ...)
+        if user then
+            local base_ext = user:base()
+            if base_ext and base_ext.is_local_player then
+                if user:camera() then -- Camera extension is present, run original function because it won't crash
+                    return original(self, weapon, user, is_menu, ...)
+                end
+                -- Here in Vanilla, it will crash hard, so return default values; copy of the logic from the function itself
+                local data = {
+                    prev_weapon_rot = Rotation()
+                }
+                local movement = user:movement()
+                if movement then
+                    data.update_type = "simulate_ingame_upd_m"
+                    data.user_unit = user
+                    data.user_m_pos = movement:m_head_pos()
+                    self:set_common_mov_data(weapon, data)
+                else
+                    data.update_type = "simulate_ingame_no_user"
+                end
+                return data
             end
-            -- Here in Vanilla, it will crash hard, so return default values; copy of the logic from the function itself
-            local data = {
-                prev_weapon_rot = Rotation()
-            }
-            local movement = user:movement()
-            if movement then
-                data.update_type = "simulate_ingame_upd_m"
-                data.user_unit = user
-                data.user_m_pos = movement:m_head_pos()
-                self:set_common_mov_data(weapon, data)
-            else
-                data.update_type = "simulate_ingame_no_user"
-            end
-            return data
         end
         return original(self, weapon, user, is_menu, ...)
     end
