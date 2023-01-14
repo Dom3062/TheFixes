8f41d0c5d1941fe02a361371f5d7f703564e5ca0
Dom <38251670+Dom3062@users.noreply.github.com>
2023-01-14 19:15:07 +0100

    Fixed GenSec heavy shotgunners in Murkywater levels

--- groupaitweakdata.lua
+++ groupaitweakdata.lua
@@ -7,3 +7,18 @@ if not TheFixesPreventer.crash_mission_preset_groupaitweak then
         end
     end
 end
+
+if not TheFixesPreventer.fix_gensec_shotgunner_in_murkywater then
+    local _init_unit_categories = GroupAITweakData._init_unit_categories
+    function GroupAITweakData:_init_unit_categories(difficulty_index, ...)
+        _init_unit_categories(self, difficulty_index, ...)
+        if difficulty_index < 6 then
+        elseif difficulty_index < 8 then -- Death Wish
+            self.unit_categories.FBI_heavy_R870.unit_types.murkywater =
+            {
+                Idstring("units/pd2_dlc_bph/characters/ene_murkywater_heavy_shotgun/ene_murkywater_heavy_shotgun")
+            }
+        else
+        end
+    end
+end
