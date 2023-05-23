8212e0c2c6b3b9496c6b6b911e163f51438ce52f
Dom <38251670+Dom3062@users.noreply.github.com>
2023-05-23 10:48:02 +0200

    Added fix: Fixed wrong preplanning texture in briefing in Firestarter Day 3

--- preplanningtweakdata.lua
+++ preplanningtweakdata.lua
@@ -2,4 +2,5 @@ local original = PrePlanningTweakData.init
 function PrePlanningTweakData:init(...)
 	original(self, ...)
 	self.locations.branchbank.mission_briefing_texture = "guis/textures/pd2/pre_planning/mission_briefing_branchbank"
+	self.locations.firestarter_3.mission_briefing_texture = "guis/textures/pd2/pre_planning/mission_briefing_branchbank"
 end
