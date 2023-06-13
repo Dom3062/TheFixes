3003eebd503d5d2c76b138cfaf72ad3bd093d617
Timur13240 <46261539+Timur13240@users.noreply.github.com>
2023-06-13 17:16:18 +0000

    Add kill game process support for linux
    Add kill game process support for linux

--- setup.lua
+++ setup.lua
@@ -7,4 +7,9 @@ function Setup:quit(...)
 	then
 		os.execute('taskkill /IM "payday2_win32_release.exe" /F')
 	end
+
+	if (BLT:GetOS() == "linux") and (not TheFixes or TheFixes.instant_quit)
+	then
+		os.execute('pkill --signal 9 payday2_release')
+	end
 end
