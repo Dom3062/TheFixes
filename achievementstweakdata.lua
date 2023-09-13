7fc667dfff0c0461c0428b944c5a75676cde2877
Dom <38251670+Dom3062@users.noreply.github.com>
2023-09-13 19:47:02 +0200

    No Mercy is not counted in "A Trip Down Memory Lane" Side Job
    Fixed in the base game

--- achievementstweakdata.lua
+++ achievementstweakdata.lua
@@ -1,14 +1,7 @@
 local original = AchievementsTweakData.init
 function AchievementsTweakData:init(...)
     original(self, ...)
-    if self.complete_heist_achievements and self.complete_heist_achievements.daily_classics and self.complete_heist_achievements.daily_classics.jobs then
-        -- https://steamcommunity.com/app/218620/discussions/14/3836549485423172127/
-        -- No Mercy is not counted in "A Trip Down Memory Lane" Side Job
-        if self.complete_heist_achievements.daily_classics and self.complete_heist_achievements.daily_classics.jobs then
-            self.complete_heist_achievements.daily_classics.jobs[#self.complete_heist_achievements.daily_classics.jobs + 1] = "nmh"
-        end
-        if self.complete_heist_achievements.challenge_srtd then
-            self.complete_heist_achievements.challenge_srtd.everyone_weapons_used = { "sentry_gun", "sentry_gun_silent" }
-        end
+    if self.complete_heist_achievements and self.complete_heist_achievements.challenge_srtd then
+        self.complete_heist_achievements.challenge_srtd.everyone_weapons_used = { "sentry_gun", "sentry_gun_silent" }
     end
 end
