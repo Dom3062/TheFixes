0e5aa07befb138b620eb3dc3567e0b972828db64
Dom <38251670+Dom3062@users.noreply.github.com>
2023-09-13 18:41:33 +0200

    Removed fix: basenetworksession.lua"]:1757: attempt to index local peer (a nil value)
    Fixed in the base game

--- basenetworksession.lua
+++ basenetworksession.lua
@@ -1,14 +1,3 @@
-TheFixesPreventer = TheFixesPreventer or {}
-if not TheFixesPreventer.crash_stats_received_basenetworksession then
-	-- basenetworksession.lua"]:1757: attempt to index local 'peer' (a nil value)
-	local stats_received_orig = BaseNetworkSession.on_statistics_recieved
-	function BaseNetworkSession:on_statistics_recieved(peer_id, ...)
-		if self:peer(peer_id) then
-            stats_received_orig(self, peer_id, ...)
-        end
-	end
-end
-
 -- Fixes Steam players not appearing on "Played with" tab
 -- Only happens on Steam with EOS Matchmaking
 if SystemInfo:distribution() == Idstring("STEAM") and SystemInfo:matchmaking() == Idstring("MM_EPIC") then
